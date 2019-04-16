015731ada1fdc18aee0e5f1c2d6d8bdb96d0e6c4
Ben Dews <contact@bendews.com>
2019-04-16 12:34:02 +1000

    Added Coffee Cup and Coffee Pot, both of which are liquid container objects

---
+++ python/coffee_cup/__init__.py
@@ -0,0 +1,16 @@
+"""A representation of a cup, designed for coffee."""
+from python.container import LiquidContainer
+
+class CoffeeCup(LiquidContainer):
+    """A representation of a cup, designed for coffee."""
+
+    def __init__(self):
+        super().__init__(self)
+
+    def drink(self, amount=20):
+        """Drinks contents of cup."""
+        self.volume -= amount
+
+    def refill(self, amount=100):
+        """Fills contents of cup."""
+        self.volume += amount
---
+++ python/coffee_pot/__init__.py
@@ -0,0 +1,16 @@
+"""A representation of a coffee pot."""
+from python.container import LiquidContainer
+
+class CoffeePot(LiquidContainer):
+    """A representation of a pot, designed for brewing coffee."""
+
+    def __init__(self):
+        super().__init__(self)
+
+    def pour(self, amount=100):
+        """Pours contents of pot."""
+        self.volume -= amount
+
+    def make(self, amount=100):
+        """Makes an amount of Coffee in the pot."""
+        self.volume += amount

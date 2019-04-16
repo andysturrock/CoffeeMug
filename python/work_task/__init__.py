1f939512bb5d25e634c913ebe3a92adff7f09b24
Ben Dews <contact@bendews.com>
2019-04-16 14:07:33 +1000

    Added Work Task object

---
+++ python/work_task/__init__.py
@@ -0,0 +1,42 @@
+"""A representation of a pot, designed for brewing coffee."""
+from random import randint
+
+
+class WorkTask(object):
+    """A representation of a pot, designed for brewing coffee."""
+
+    def __init__(self):
+        self._completed = False
+        self._total_effort = randint(2, 5)
+        self._effort = self._total_effort
+
+    @property
+    def done(self):
+        """Returns completed status of task."""
+        return self._completed
+
+    @done.setter
+    def done(self, value):
+        """Returns completed status of task."""
+        if value and self.effort != 0:
+            raise ValueError(
+                "Task can not be completed when effort is remaining")
+        self._completed = value
+
+    @property
+    def effort(self):
+        """Returns the amount of effort remaining on the task."""
+        return self._effort
+
+    @effort.setter
+    def effort(self, value):
+        """Sets the amount of effort remaining on the task."""
+        self._effort = value
+        if self._effort > self._total_effort:
+            raise ValueError("Effort can't be more than total effort")
+        if self._effort <= 0:
+            self.done = True
+
+    def execute(self, effort=1):
+        """Expends effort towards completing a task."""
+        self.effort -= effort

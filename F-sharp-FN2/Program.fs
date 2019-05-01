644ab3c1fd5409c96cd76a45452ff07671a2d43b
Lex Mitchell <lexmitchell@live.com>
2019-05-01 20:05:23 +0100

    Added F# functional sample that uses a one step state machine

---
+++ F-sharp-FN2/Program.fs
@@ -0,0 +1,19 @@
+﻿open System
+
+type Coffee     = HasCoffee | NoCoffee
+let isTrue ()   = Random().Next(0,2) = 0
+let refill      = HasCoffee
+let getCup      = if isTrue() then NoCoffee else HasCoffee
+let workDone () = isTrue()
+let check cup   =  
+    match cup with
+     | HasCoffee -> HasCoffee 
+     | NoCoffee  -> refill
+let drink cup   = printf "Ahh coffee"
+let doWork ()   = printf "I'm working on it!"
+
+[<EntryPoint>]
+let main args =
+    while not <| workDone() do
+        getCup |> check |> drink |> doWork
+    0
